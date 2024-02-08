ce84bda8f4ddf09678bc1b082c76a91510d68dd3
Omkar <omkarnagarkar55@gmail.com>
2024-02-07 18:14:52 -0800

    Added solution for LCA in BT

---
+++ Day18_BinaryTrees-II/5-LCA-In-BT.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        # If root is none or if root is equal to p or q then return root
+        if root == None or root == p or root == q:
+            return root
+        left = self.lowestCommonAncestor(root.left,p,q)
+        right = self.lowestCommonAncestor(root.right,p,q)
+
+        if left == None:
+            return right
+        elif right == None:
+            return left
+        else:
+            # Both left and write are not None
+            return root
+        
